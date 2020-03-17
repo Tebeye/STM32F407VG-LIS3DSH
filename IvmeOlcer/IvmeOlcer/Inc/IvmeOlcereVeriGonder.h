810ce4d09c098a271f2823aeb76f59024600fff7
Türker Berke Yıldırım <tberkeyildirim@gmail.com>
2020-03-18 01:14:01 +0300

    HaberlesmeDeneme
    İvmeÖlçere erişildi ve ilk haberleşme sağlandı.

---
+++ IvmeOlcer/IvmeOlcer/Inc/IvmeOlcereVeriGonder.h
@@ -0,0 +1,21 @@
+/*
+ * IvmeOlcereVeriGonder.h
+ *
+ *  Created on: 4 Mar 2020
+ *      Author: Berke
+ */
+
+#ifndef IVMEOLCEREVERIGONDER_H_
+#define IVMEOLCEREVERIGONDER_H_
+
+void IvmeOlcereVeriGonder(uint8_t *gonderilecekAdres,
+					 uint8_t *gonderilecekVeri)
+{
+
+
+
+
+
+}
+
+#endif /* IVMEOLCEREVERIGONDER_H_ */
